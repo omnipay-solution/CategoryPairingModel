757202cfc08d9442045288e33852aeb97890eef5
AshishD0d <ashish.dod@omnipay-solution.com>
2025-06-28 18:43:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -93,5 +93,5 @@ def recommend(request: CategoryRequest):
 # === ✅ Add this block to run the app properly on Render ===
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
